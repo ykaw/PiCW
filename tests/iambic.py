ec34cf477cfeecca6fc7eedd98bb2d6cd4023b1b
KAWAMATA Yoshihiro <kaw@nimbus25.localnet>
2018-01-15 09:47:20 +0900

    iambic.py - worked

--- tests/iambic.py
+++ tests/iambic.py
@@ -19,8 +19,8 @@ pi.set_mode(port_dit, pigpio.INPUT)
 pi.set_mode(port_dah, pigpio.INPUT)
 pi.set_pull_up_down(port_dit, pigpio.PUD_UP)
 pi.set_pull_up_down(port_dah, pigpio.PUD_UP)
-pi.set_glitch_filter(port_dit, 1000)
-pi.set_glitch_filter(port_dah, 1000)
+pi.set_glitch_filter(port_dit, 3000)
+pi.set_glitch_filter(port_dah, 3000)
 
 port_pwm=18
 pwm_freq= 500
@@ -31,48 +31,76 @@ pi.set_PWM_dutycycle(port_pwm, 0)
 
 # handlings for dits and dahs
 #
-wpm=3
+wpm=20
 ditlen=60/(50*wpm)
 
 # global status
 # and notifying event
 #
-dit_mark=False
-dah_mark=False
-evt_mark=0
-sqz_mark=0
+dit_mark =False
+dah_mark =False
+evt_mark =0
+sqz_marks=[]
 
 ev=threading.Event()
 
 # subthread for iambic output
 #
 def keying_iambic():
-    global dit_mark, dah_mark, evt_mark, sqz_mark
+    def output_with_squeezed(mark):
+        def mark_and_space(mark, space):
+            global pi
+            global sqz_marks
+            sqz_marks=[]
+            pi.set_PWM_dutycycle(port_pwm, 128)
+            time.sleep(mark)
+            saved_sqz_marks=sqz_marks
+            pi.set_PWM_dutycycle(port_pwm, 0)
+            time.sleep(space)
+            sqz_marks=saved_sqz_marks
+        global ditlen
+        global sqz_marks
+        if mark==1:
+            alt_mark=2
+            mark_and_space(ditlen, ditlen)
+        elif mark==2:
+            alt_mark=1
+            mark_and_space(3*ditlen, ditlen)
+        while sqz_marks:
+            mark=sqz_marks.pop(0)
+            if mark==1:
+                alt_mark=2
+                mark_and_space(ditlen, ditlen)
+            elif mark==2:
+                alt_mark=1
+                mark_and_space(3*ditlen, ditlen)
+        return(alt_mark)
+
+    global ditlen
+    global dit_mark, dah_mark, evt_mark, sqz_marks
     global ev
     while True:
-        print("waiting event...")
         ev.clear()
         ev.wait()
-        sqz_mark=0
-        print("----awake----------")
-        print("dit_mark:", dit_mark)
-        print("dah_mark:", dah_mark)
-        print("evt_mark:", evt_mark)
-        print("sqz_mark:", sqz_mark)
-        time.sleep(5)
-        print("----timeout--------")
-        print("dit_mark:", dit_mark)
-        print("dah_mark:", dah_mark)
-        print("evt_mark:", evt_mark)
-        print("sqz_mark:", sqz_mark)
+        next_mark=output_with_squeezed(evt_mark)
+
+        while True:
+            if dit_mark and dah_mark:
+                next_mark=output_with_squeezed(next_mark)
+            elif dit_mark:
+                next_mark=output_with_squeezed(1)
+            elif dah_mark:
+                next_mark=output_with_squeezed(2)
+            else:
+                break
 
 iambic=threading.Thread(target=keying_iambic)
 iambic.start()
 
-# general callback function
+# callback function for press/release paddles
 #
 def cb_func(port, level, tick):
-    global dit_mark, dah_mark, evt_mark, sqz_mark
+    global dit_mark, dah_mark, evt_mark, sqz_marks
     global ev
 
     # paddle pressed
@@ -83,8 +111,7 @@ def cb_func(port, level, tick):
         elif port==port_dah:
             evt_mark=2
             dah_mark=True
-        if sqz_mark==0:
-            sqz_mark=evt_mark
+        sqz_marks.append(evt_mark)
         # notify to iambic subthread
         ev.set()
 
@@ -104,8 +131,9 @@ cb_dah=pi.callback(port_dah, pigpio.EITHER_EDGE, cb_func)
 #
 try:
     while True:
-        line=input('CW : ')
-        print("--->", line)
+        line=input(str(wpm) + ' WPM : ')
+        wpm=float(line)
+        ditlen=60/(50*wpm)
     pi.set_PWM_dutycycle(port_pwm, 0)
     pi.stop()
 
